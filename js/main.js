9ce7464c67be85119309ecc623f7ace5a72b94ed
LeandroLedesma <leandro.d.ledesma@gmail.com>
2018-09-27 13:27:43 -0300

    add html, css and layout responsive

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var porcentaje = document.querySelector("#porcentaje");
+var multiplicacion = document.querySelector("#multiplicacion");
+var dos = document.querySelector("#dos");
+
+
+
+var botones = document.querySelectorAll("button");
+botones.forEach(function(btn){
+    
+    btn.addEventListener("click",function(e){
+          
+    console.log(btn.value);    
+    })
+    
+    
+    })
+    
+  
